bd149b058219cb6ff2af211bdb086d73b40510fa
annabaker <anna.baker.313@gmail.com>
2020-03-10 18:23:27 -0400

    updates

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -13,7 +13,7 @@ public class App
         String password = System.getenv("password");
 
         RedisClient redisClient = new RedisClient(
-                RedisURI.create("redis://" + password + "@192.81.218.202:6379"));
+                RedisURI.create("redis://" + password + "@my-redis-db-host:6379"));
         RedisConnection<String, String> connection = redisClient.connect();
 
         System.out.println("Connected to Redis!!");
